720875885780791a2eef28cfe8a799b8a15d80f7
Masher Shin <zeushin@gmail.com>
2017-08-02 18:45:34 +0900

    header image slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,24 @@
  */
 
 import React, { Component } from 'react';
+import Slider from 'react-slick';
 
 class Header extends Component {
     render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
         return (
-            <div><h1>Header</h1></div>
+            <Slider {...settings}>
+                <img src="image/header01.jpeg"/>
+                <img src="image/header02.jpeg"/>
+                <img src="image/header03.jpeg"/>
+                <img src="image/header04.jpeg"/>
+            </Slider>
         );
     }
 }
