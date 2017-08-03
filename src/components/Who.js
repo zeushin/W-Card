0e55e240f5c640a7e8b230b5042454025c2dfaa6
Beomcheol Shin <beomcheol.shin@rgpkorea.co.kr>
2017-08-03 16:33:52 +0900

    popup action

--- src/components/Who.js
+++ src/components/Who.js
@@ -3,12 +3,36 @@
  */
 
 import React, { Component } from 'react';
+import { ModalContainer, ModalDialog } from 'react-modal-dialog';
 
 class Who extends Component {
+    constructor() {
+        super();
+        this.state = { isShowingModal: false };
+    }
+
+    _attend() {
+        this.setState({ isShowingModal: true });
+    }
+
+    _close() {
+        this.setState({ isShowingModal: false });
+    };
+
     render() {
         return (
             <div>
                 <h1>RSVP</h1>
+                <button onClick={ this._attend.bind(this) }>Attendance</button>
+                {
+                    this.state.isShowingModal &&
+                    <ModalContainer onClose={ this._close.bind(this) } onTouchStart={ this._close.bind(this) }>
+                        <ModalDialog onClose={ this._close.bind(this) }>
+                            <h1>Dialog Content</h1>
+                            <p>More Content. Anything goes here</p>
+                        </ModalDialog>
+                    </ModalContainer>
+                }
             </div>
         );
     }
